94e457b1b688696e43f01f8e4b6e34ec114904ba
cjmccann <connor.j.mccann@gmail.com>
2014-03-13 01:38:41 -0400

    checking units

--- transit/gmaps.js
+++ transit/gmaps.js
@@ -91,8 +91,8 @@ function renderLine(aLine) {
 			marker = new google.maps.Marker({
 				position: me,
 				icon: goldStar,
-				title: "I am here at lat:" + myLat + "; long: " + myLng + "and the closest T-Stop is" + closestStop.title +
-					  "which is " + shortestDistance + " miles away."
+				title: "I am here at lat:" + myLat + "; long: " + myLng + "\nThe closest T-Stop is " + closestStop.title +
+					  " which is " + shortestDistance + " miles away."
 			});			
 			console.log(closestStop);
 		}
