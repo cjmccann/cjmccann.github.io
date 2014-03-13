67b62f1f28ee0bc484ec939df872dace6c19c991
cjmccann <connor.j.mccann@gmail.com>
2014-03-13 01:23:29 -0400

    lksajd

--- transit/gmaps.js
+++ transit/gmaps.js
@@ -8,9 +8,9 @@ var goldStar = {
 	path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
 	fillColor: 'yellow',
 	fillOpacity: 0.8,
-	scale: .2,
+	scale: .1,
 	strokeColor: 'gold',
-	strokeWeight: 3
+	strokeWeight: 2
 };
 var closestStop;
 var shortestDistance;
